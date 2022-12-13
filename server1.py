2a7b1a964aa3e5f06c1ba531cddb138ba686c406
denixx-dev <denissm2002@yandex.ru>
2022-12-14 01:24:37 +0300

    socket interaction between

--- server1.py
+++ server1.py
@@ -1,7 +1,65 @@
-import tkinter as tk
+from tkinter import *
+import socket
+def server1_socket():
+    window = create_window()
 
-window = tk.Tk()
-window.title("Server 1")
-window.geometry('400x250')
-window.mainloop()
+    HOST = "127.0.0.1"
+    PORT = 65432
 
+
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #     s.bind((HOST, PORT))
+    #     s.listen()
+    #     conn, addr = s.accept()
+    #     with conn:
+    #         label2 = Label(window, text = f"Connected by {addr}")
+    #         label2.grid(column=0, row=1)
+    #         while True:
+    #             data = conn.recv(1024)
+    #             if not data:
+    #                 break
+    #             conn.sendall(data)
+
+def create_window():
+    HOST = "127.0.0.1"
+    PORT = 65432
+
+    window = Tk()
+    window.title("Server 1")
+    window.geometry('400x250')
+
+    label1 = Label(window, text = "Sending a message...")
+    label1.grid(column=0, row=0)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            label2 = Label(window, text = f"Connected by {addr}")
+            label2.grid(column=0, row=1)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+    window.mainloop()
+
+# server1_socket()
+# create_window()
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 8888  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
