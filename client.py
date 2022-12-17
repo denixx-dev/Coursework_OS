289cc35a22e100ef45ff8dc31784df33d6a64f67
denixx-dev <denissm2002@yandex.ru>
2022-12-17 17:57:16 +0300

    client interaction

--- client.py
+++ client.py
@@ -13,14 +13,13 @@ def create_window():
     window.mainloop()
 
 
-import socket
-
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 8888  # The port used by the server
+PORT = 8000  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((HOST, PORT))
+    while True:
+        data = client.recv(1024)
+        print(f"{data}".encode("utf-8"))
+        client.sendto("Client connected".encode("utf-8"), (HOST, PORT))
 
-print(f"Received {data!r}")
--- server1.py
+++ server1.py
@@ -1,65 +1,31 @@
 from tkinter import *
 import socket
-def server1_socket():
-    window = create_window()
-
-    HOST = "127.0.0.1"
-    PORT = 65432
-
-
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #     s.bind((HOST, PORT))
-    #     s.listen()
-    #     conn, addr = s.accept()
-    #     with conn:
-    #         label2 = Label(window, text = f"Connected by {addr}")
-    #         label2.grid(column=0, row=1)
-    #         while True:
-    #             data = conn.recv(1024)
-    #             if not data:
-    #                 break
-    #             conn.sendall(data)
-
-def create_window():
-    HOST = "127.0.0.1"
-    PORT = 65432
-
-    window = Tk()
-    window.title("Server 1")
-    window.geometry('400x250')
-
-    label1 = Label(window, text = "Sending a message...")
-    label1.grid(column=0, row=0)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            label2 = Label(window, text = f"Connected by {addr}")
-            label2.grid(column=0, row=1)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
-
-    window.mainloop()
 
 # server1_socket()
 # create_window()
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 8888  # Port to listen on (non-privileged ports are > 1023)
+PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+window = Tk()
+window.title("Server 1")
+window.geometry('400x250')
+
+window.update_idletasks()
+
+label1 = Label(window, text = "Waiting for client to connect")
+label1.grid(column=0, row=0)
+
+window.update_idletasks()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server1:
+    server1.bind((HOST, PORT))
+    server1.listen()
+
+    while True:
+        user, address = server1.accept()
+
+        user.send("Client connected to server 1".encode("utf-8"))
+        window.mainloop()
+window.mainloop()
+
